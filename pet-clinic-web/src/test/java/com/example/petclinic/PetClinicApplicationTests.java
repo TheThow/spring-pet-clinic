0ec300fb3ee89b0e2146d487b96e3cbaba0bfcad
Thomas Wiest <t.wiest@reply.de>
2020-08-21 08:50:00 +0200

    Split into multi-module project

--- pet-clinic-web/src/test/java/com/example/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/example/petclinic/PetClinicApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
 
 @SpringBootTest
 class PetClinicApplicationTests {
